d8f66cb8244bc3b8cf10ee1bdc5a7f5f2c1128fa
Jesús García Sanchéz <jesusangelgarciasanchez@MacBook-Air-de-Jesus.local>
2019-06-17 18:00:25 +0200

    sound

--- main.js
+++ main.js
@@ -12,11 +12,31 @@
 
 window.onload = function(){
 
+//VARIABLE SONIDO
+let audio = new Audio("./audio/Game 4.mp3");
+audio.play()
 
 
 
 
+  
+
+//silenciar o reproducir sonido
+function muteSound() {
+    if (audio.paused) {
+      audio.play();
+      $(".audio").attr("src", "./img/music on.svg");
+    } else {
+      audio.pause();
+      $(".audio").attr("src", "./img/music off.svg");
+    }
+  }
 
 
-    
+
+ $(".audio").click(function() {
+   muteSound();
+ });
+
+
 }
